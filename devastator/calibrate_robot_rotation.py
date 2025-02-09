a4e08beea54833aeeee6e2fcbc7d6b9ee54ee9c7
Nawal Husnoo <nawal@husnoo.com>
2025-02-09 10:53:42 +0000

    got some numbers to calibrate

--- devastator/calibrate_robot_rotation.py
+++ devastator/calibrate_robot_rotation.py
@@ -14,10 +14,17 @@ Calculate and plot degrees/second for each rotation speed.
 import datetime
 import time
 import numpy
+import pylab
 
 from src.robot import Robot
 
 
+def analysis():
+    speeds = numpy.arange(0.6,1.01,0.1)
+    counts = numpy.array([5,5,10,20,20])
+    ccw_time = [147.137595,  41.266779,  45.56845,   59.802386,  43.389424]
+    cw_time = [149.367774,  40.58986,   43.930003,  58.371789,  42.395014]
+    pylab.ion()
 
 
 
@@ -26,26 +33,34 @@ def main():
     robot = Robot()
     print('START')
 
-    speeds = numpy.arange(0.2,1.01,0.2)
+    # Something weird - 0.2 is quite fast, 0.4,0.6 are slow, 0.8 and 1.0 are very fast.
+    # 0.5 seems to stall, 0.6 is very slow
+    # so maybe calibrate around 0.6-1.0
+    # also 20 rotations at 0.6 will take forever. Let's just do 5.
+    
+    
+    speeds = numpy.arange(0.6,1.01,0.1)
+    counts = numpy.array([5,5,10,20,20])
+    
     time_cw = numpy.zeros(len(speeds))
     time_ccw = numpy.zeros(len(speeds))
     for i, rotation_speed in enumerate(speeds):
+        print(f'rotation_speed: {rotation_speed}, CCW')
+        input('Press Enter to begin...')
+        start = datetime.datetime.now()
+        robot.spin_ccw(rotation_speed)
+        input(f'Press Enter when {counts[i]} rotations have happened')
+        stop = datetime.datetime.now()
+        time_ccw[i] = (stop-start).total_seconds()
+        robot.stop()
+
         print(f'rotation_speed: {rotation_speed}, CW')
         input('Press Enter to begin...')
         start = datetime.datetime.now()
         robot.spin_cw(rotation_speed)
-        input('Press Enter when 20 rotations have happened')
+        input(f'Press Enter when {counts[i]} rotations have happened')
         stop = datetime.datetime.now()
         time_cw[i] = (stop-start).total_seconds()
-        robot.stop()
-
-        print(f'rotation_speed: {rotation_speed}, CCW')
-        input('Press Enter to begin...')
-        start = datetime.datetime.now()
-        robot.spin_cw(-rotation_speed)
-        input('Press Enter when 20 rotations have happened')
-        stop = datetime.datetime.now()
-        time_ccw[i] = (stop-start).total_seconds()
         robot.stop()
         
         print('---')
@@ -53,7 +68,7 @@ def main():
     print(time_cw, time_ccw)
 
 
-if __name__=='__main__':
-    main()
+#if __name__=='__main__':
+#    main()
 
 
