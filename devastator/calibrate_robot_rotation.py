eeb1c3d772dfcef7a0c376078a18660c6ecca1d0
Nawal Husnoo <nawal@husnoo.com>
2025-02-09 10:23:45 +0000

    simple script to calibrate rotation

--- devastator/calibrate_robot_rotation.py
+++ devastator/calibrate_robot_rotation.py
@@ -2,8 +2,8 @@
 Calibrate robot's rotation
 
 
->>> from src.robot import Robot
->>> robot = Robot()
+>>> 
+>>> 
 >>> robot.spin_cw(0.5)
 >>> robot.spin_ccw(0.5)
 
@@ -11,14 +11,43 @@ Over the range of speeds from 0 to 1, say in steps of 0.2, count how long it tak
 Calculate and plot degrees/second for each rotation speed.
 """
 
+import datetime
+import time
+import numpy
+
+#from src.robot import Robot
 
 
+def main():
+    #robot = Robot()
+    print('START')
+
+    speeds = numpy.arange(0.2,1.01,0.2)
+    time_cw = numpy.zeros(len(speeds))
+    time_ccw = numpy.zeros(len(speeds))
+    for i, rotation_speed in enumerate(speeds):
+        print(f'rotation_speed: {rotation_speed}, CW')
+        input('Press Enter to begin...')
+        start = datetime.datetime.now()
+        #robot.spin_cw(rotation_speed)
+        input('Press Enter when 20 rotations have happened')
+        stop = datetime.datetime.now()
+        time_cw[i] = (stop-start).total_seconds()
+
+        print(f'rotation_speed: {rotation_speed}, CCW')
+        input('Press Enter to begin...')
+        start = datetime.datetime.now()
+        #robot.spin_cw(-rotation_speed)
+        input('Press Enter when 20 rotations have happened')
+        stop = datetime.datetime.now()
+        time_ccw[i] = (stop-start).total_seconds()
+        
+        print('---')
+
+    print(time_cw, time_ccw)
 
 
-
-
-
-
-
+if __name__=='__main__':
+    main()
 
 
